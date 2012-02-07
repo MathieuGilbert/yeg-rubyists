4d2db4bee4dbb1b1a9f2bcd14c3cd7b4c13672d4
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-06 19:43:07 -0700

    update devise config:

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,1 +1,21 @@
-require 'devise/orm/active_record'
+Devise.setup do |config|
+  require 'devise/orm/active_record'
+  
+  # Configure which authentication keys should be case-insensitive.
+  # These keys will be downcased upon creating or modifying a user and when used
+  # to authenticate or find a user. Default is :email.
+  config.case_insensitive_keys = [ :email ]
+  
+  # Configure which authentication keys should have whitespace stripped.
+  # These keys will have whitespace before and after removed upon creating or
+  # modifying a user and when used to authenticate or find a user. Default is :email.
+  config.strip_whitespace_keys = [ :email ]
+  
+  # Configure which authentication keys should have whitespace stripped.
+  # These keys will have whitespace before and after removed upon creating or
+  # modifying a user and when used to authenticate or find a user. Default is :email.
+  config.strip_whitespace_keys = [ :email ]
+  
+  # The default HTTP method used to sign out a resource. Default is :delete.
+  config.sign_out_via = :delete
+end
