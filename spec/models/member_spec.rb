8f938ba29fa3fecbadb7d6bdc9d2a09e10140466
Mathieu Gilbert <matnarak@gmail.com>
2012-02-07 20:57:30 -0700

    added more member model specs

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Member do
   before(:each) do
-    @attr = { :name => "Test User",
-              :email => "member@example.com",
-              :password => "password",
+    @attr = { :name                  => "Test User",
+              :email                 => "member@example.com",
+              :password              => "password",
               :password_confirmation => "password",
-              :twitter => "http://www.twitter.com",
-              :github => "http://www.github.com",
-              :blogrss => "http://www.google.com" }
+              :twitter               => "http://www.twitter.com",
+              :github                => "http://www.github.com",
+              :blogrss               => "http://www.google.com" }
   end
   
   it "should create a member given valid attributes" do
@@ -19,8 +19,26 @@ describe Member do
     Member.new(@attr.merge(:name => "")).should_not be_valid
   end
   
+  it "should require at least 1 social link" do
+    Member.new(@attr.merge(:twitter => "",
+                           :github  => "",
+                           :blogrss => "")).should_not be_valid
+  end
+  
   describe "email validation" do
+    it "should accept valid email addresses" do
+      addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+      addresses.each do |address|
+        Member.new(@attr.merge(:email => address)).should be_valid
+      end
+    end
     
+    it "should reject invalid email addresses" do
+      addresses = %w[user@foo,com THE_USER_at_foo.bar.org first.last@foo]
+      addresses.each do |address|
+        Member.new(@attr.merge(:email => address)).should_not be_valid
+      end
+    end
   end
   
   describe "password validation" do
@@ -32,9 +50,5 @@ describe Member do
       Member.new(@attr.merge(:password_confirmation => "nomatch")).should_not be_valid
     end
   end
-
-  
-  
-  
 
 end
