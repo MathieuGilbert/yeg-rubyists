550ff6035af8cd96c1bf87249b19ff9cd50b7802
Mathieu Gilbert <matnarak@gmail.com>
2012-02-07 20:23:28 -0700

    added tests

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper'
 
 describe Member do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :name => "Test User",
+              :email => "member@example.com",
+              :password => "password",
+              :password_confirmation => "password",
+              :twitter => "http://www.twitter.com",
+              :github => "http://www.github.com",
+              :blogrss => "http://www.google.com" }
+  end
+  
+  it "should create a member given valid attributes" do
+    Member.create!(@attr)
+  end
+
+  it "should require a name" do
+    Member.new(@attr.merge(:name => "")).should_not be_valid
+  end
+  
+  describe "email validation" do
+    
+  end
+  
+  describe "password validation" do
+    it "should reject blank passwords" do
+      Member.new(@attr.merge(:password => "")).should_not be_valid
+    end
+    
+    it "should reject non-matching passwords" do
+      Member.new(@attr.merge(:password_confirmation => "nomatch")).should_not be_valid
+    end
+  end
+
+  
+  
+  
+
 end
