ddeae63c9fe4bc509fc5da074104875326c905a8
Mathieu Gilbert <matnarak@gmail.com>
2012-02-09 21:52:01 -0700

    updated members controller spec, removed spec/views

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -5,21 +5,34 @@ describe MembersController do
     @member = Factory(:member)
   end
   
-
-  describe "administrator access" do
-    it "should deny non admins access to /admin" do
-      sign_in(@member)
+  describe "public users" do
+    it "should protect the admin page" do
       get :administer
       response.should redirect_to(root_path)
-    end
-    
-    it "should allow admins to access /admin" do
-      @member.toggle!(:admin)
-      sign_in(@member)
-      get :administer
-      response.should be_success
-    end
-    
+    end  
   end
+  
+  describe "logged-in members" do
+    before(:each) do
+      sign_in(@member)
+    end
+    
+    describe "non-admins" do
+      it "should deny non admins access to /admin" do
+        get :administer
+        response.should redirect_to(root_path)
+      end
+    end
+    
+    describe "admins" do
+      it "should allow admins to access /admin" do
+        @member.toggle!(:admin)
+        get :administer
+        response.should be_success
+      end
+    end
+  end
+
+
   
 end
