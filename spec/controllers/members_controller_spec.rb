3cf42183f29b30ee9c6529652045809eca3f28ce
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-09 22:03:42 -0700

    merge spec

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -5,15 +5,8 @@ describe MembersController do
     @member = Factory(:member)
   end
   
-<<<<<<< HEAD
-
-  describe "administrator access" do
-    it "should be denied to regular members" do
-      sign_in(@member)
-=======
   describe "public users" do
     it "should protect the admin page" do
->>>>>>> 7b3be4741773c98028e0ad29867dd58fe7f69374
       get :administer
       response.should redirect_to(root_path)
     end  
@@ -24,19 +17,11 @@ describe MembersController do
       sign_in(@member)
     end
     
-<<<<<<< HEAD
-    it "should allow be granted to administrators" do
-      @member.toggle!(:admin)
-      sign_in(@member)
-      get :administer
-      response.should be_success
-=======
     describe "non-admins" do
       it "should deny non admins access to /admin" do
         get :administer
         response.should redirect_to(root_path)
       end
->>>>>>> 7b3be4741773c98028e0ad29867dd58fe7f69374
     end
     
     describe "admins" do
