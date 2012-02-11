83783f218b9cfbfcbb89bfc4a454691edc199e65
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-11 14:03:42 -0700

    tweet time

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,9 @@ class TweetsController < ApplicationController
   respond_to :json
   
   def index
+    # need to be able to handle something like this:
+    # return ('/tweets?from_time=' + this.models[this.models.length-1].get("created_at"));
+    
     @tweets = Tweet.all
     
     respond_with(@tweets) do |format|
