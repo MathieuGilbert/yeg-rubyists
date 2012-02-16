48b8fb284e1b5a410573bd02c4d7511dd0afaa12
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-15 17:35:37 -0700

    added more to dataparser, getting closer to have tweets parsed

--- app/models/data_parser.rb
+++ app/models/data_parser.rb
@@ -16,50 +16,50 @@ class DataParser
   end
 
   def self.update_tweets(members)
-    if members.empty?
-      begin
+    if !members.empty?
         # grab the most recent tweet
-        last_updated_tweet = Tweets.order("since_id desc").first
-        
+        last_updated_tweet = Tweet.order("since_id desc").first
+
         # grab the newest tweets from the yeg-members list
-        twitter_feed = RestClient::Resource.new "https://api.twitter.com/1/yeg_rubyists/lists/yegrb-members/statuses.atom?&include_rts=true&count=10&since_id=#{last_updated_tweet.since_id}"
-        SimpleRSS.parse ( twitter_feed.get )
+        twitter_rest = RestClient::Resource.new "https://api.twitter.com/1/yeg_rubyists/lists/yegrb-members/statuses.atom?&include_rts=true&count=10&since_id=#{last_updated_tweet.since_id}"
+        twitter_feed = SimpleRSS.parse ( twitter_rest.get )
 
         # loop through each newest tweet
         twitter_feed.items.each do |new_tweet|
-          
           # compare the tweet user name vs the new tweet username
+          
           members.each do |member|
-            puts new_tweet.author
+            tweet_found = false
             
-
+            # break up the username and content from the tweet
+            # i think there's an easier way to do this, assign multiple vars with regex groups
+            tweet_breakup = new_tweet.title.scan(/(^.*):(.*)/i)
+            username = tweet_breakup[0][0]
+            content = tweet_breakup[0][1]
+            
+            # check if usernames are equal (case insensitive)
+            if member.twitter.casecmp(username)
+              
+              # now that we know who it belongs to we need to parse the rest of the data
+              # since_id & url => <id>tag:twitter.com,2007:http://twitter.com/RyanOnRails/statuses/169502418399264769</id>
+              # date => <published>2012-02-14T19:25:00+00:00</published>
+              
+              # create tweet
+              member.tweet.create!({
+                :date    => new_tweet.published,
+                :content => content,
+                :url     => new_tweet.id});
+                
+              tweet_found = true
+            end
+            
+            # if a match is found for the tweet break out of this loop
+            break if tweet_found == true
           end
         end
-# 
-          # current_tweets.items.each do |current_tweet|
-            # # check if the tweet exists
-            # if current_tweet.title == new_tweet.title
-              # tweet_found = true
-            # end
-          # end
-          # puts 'tweet ' + tweet_found
-          # # tweet wasn't found, add it to the db
-          # if tweet_found == false
-            # puts 'Adding Tweet'
-            # member.tweet.create!({
-              # :date    => new_tweet.published,
-              # :content => new_tweet.title,
-              # :url     => new_tweet.id
-            # });
-          # end
-        # end  
-
-      rescue
-        # 404 -> twitter handle not found
-        # 400 -> reached limit of 150 an hour
-      end
+        
+    nil    # make sure to have begin rescue
     end 
-    
   end
 
 end
