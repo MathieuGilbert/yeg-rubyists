86da5f6bd702832943eaa5d8c6ed67c350d16cd8
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-16 12:21:13 -0700

    updated dataparser

--- app/models/data_parser.rb
+++ app/models/data_parser.rb
@@ -16,37 +16,43 @@ class DataParser
   end
 
   def self.update_tweets(members)
+    # make sure we have members to match against
     if !members.empty?
         # grab the most recent tweet
         last_updated_tweet = Tweet.order("since_id desc").first
-
-        # grab the newest tweets from the yeg-members list
-        twitter_rest = RestClient::Resource.new "https://api.twitter.com/1/yeg_rubyists/lists/yegrb-members/statuses.atom?&include_rts=true&count=10&since_id=#{last_updated_tweet.since_id}"
-        twitter_feed = SimpleRSS.parse ( twitter_rest.get )
+        
+        # If there are no since_id's in the db, we go and get all of the possible tweets
+        if last_updated_tweet.nil?
+          twitter_rest = RestClient::Resource.new "https://api.twitter.com/1/yeg_rubyists/lists/yegrb-members/statuses.atom?&include_rts=true"
+          twitter_feed = SimpleRSS.parse ( twitter_rest.get )
+        else
+          # grab the newest tweets from the yeg-members list based on the since_id
+          twitter_rest = RestClient::Resource.new "https://api.twitter.com/1/yeg_rubyists/lists/yegrb-members/statuses.atom?&include_rts=true&since_id=#{last_updated_tweet.since_id}"
+          twitter_feed = SimpleRSS.parse ( twitter_rest.get )
+        end
 
         # loop through each newest tweet
         twitter_feed.items.each do |new_tweet|
-          # compare the tweet user name vs the new tweet username
+          # break up the username and content from the tweet
+          # i think there's an easier way to do this, assign multiple vars with regex groups
+          username, content = new_tweet.title.match(/(^[^\:]*): (.*)/i).captures
           
+          # compare the tweet user name vs the new tweet username
           members.each do |member|
             tweet_found = false
-            
-            # break up the username and content from the tweet
-            # i think there's an easier way to do this, assign multiple vars with regex groups
-            username, content = new_tweet.title.match(/(^.*): (.*)/i).captures
-            
+
             # check if usernames are equal (case insensitive)
-            if member.twitter.casecmp(username)
-              
+            if member.twitter.casecmp(username) == 0
+
               # pull out url, since_id and published_at
-              url, since_id = new_tweet.id.match(/(h.*\/)(.*)/).captures
+              url, since_id = new_tweet.id.match(/(http.*\/)(.*)/).captures
               published_dt = Time.parse(new_tweet.published.to_s)
               
               # create tweet
               member.tweets.create!({
                 :date     => published_dt,
                 :content  => content,
-                :url      => url,
+                :url      => url + since_id.to_s,
                 :since_id => since_id})
                 
               tweet_found = true
