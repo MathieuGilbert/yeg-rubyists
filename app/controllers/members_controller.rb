1e80fdd819a242d6b120e137dd84bb57757f20e5
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-16 21:06:52 -0700

    added twitter approval, twitter 401 in controller, still issues

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -11,13 +11,39 @@ class MembersController < ApplicationController
   def approve
     # approve the member
     member = Member.find(params[:id])
+  
+    # add the member to the twitter list
+    if !member.twitter.empty?
+      add_to_twitter_list(member)
+    end
+    
+    # github here
+    if !member.github.empty?
+
+    end
+    
+    # blog here
+    if !member.blogrss.empty?
+
+    end
+    
     member.update_attributes({:status => 'approved'})
+    
     redirect_to(admin_path)
   end
 
   private
     def admin_member
       redirect_to(root_path) unless current_member.admin?
+    end
+    
+    def add_to_twitter_list(member, twitter)
+      # grab the twitter list
+      twitter = Twitter::Client.new
+      twitter_list = twitter.list_timeline('yegrb-members')
+      
+      # add twitter user to yeg-members list to start monitoring tweets
+      twitter.list_add_member('yegrb-members', member.twitter)
     end
     
 end
