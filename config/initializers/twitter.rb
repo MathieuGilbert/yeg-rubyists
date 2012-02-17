6f0385e52bbe8938d80fc7bf1b1d75c58c895c07
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-16 17:49:27 -0700

    updated for the tiwtterz

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = '1Lo6jhiT8eMJwiko1i6auQ'
+  config.consumer_secret = 'v1BBLc5VtK9fg3EoxC0gwMsXlzqInqLqCIUNtgJkjnM'
+  config.oauth_token = '493485035-W33VcB7rSgEQeQr5YQh9ETy77il4aemsnG9XdH4x'
+  config.oauth_token_secret = '0hSsREW6ghvWoAbogHjZ2lGSi216o0To9Prd69nJIs'
+end
