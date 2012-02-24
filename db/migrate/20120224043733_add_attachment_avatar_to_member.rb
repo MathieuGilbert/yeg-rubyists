d13407b3fc35accc9e7795abe383a414121eb563
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-23 22:42:43 -0700

    added ability for member to upload avatar, imagemagick needs to be installed

---
+++ db/migrate/20120224043733_add_attachment_avatar_to_member.rb
@@ -0,0 +1,15 @@
+class AddAttachmentAvatarToMember < ActiveRecord::Migration
+  def self.up
+    add_column :members, :avatar_file_name, :string
+    add_column :members, :avatar_content_type, :string
+    add_column :members, :avatar_file_size, :integer
+    add_column :members, :avatar_updated_at, :datetime
+  end
+
+  def self.down
+    remove_column :members, :avatar_file_name
+    remove_column :members, :avatar_content_type
+    remove_column :members, :avatar_file_size
+    remove_column :members, :avatar_updated_at
+  end
+end
