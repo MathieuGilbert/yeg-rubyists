ca34206349c608aca35b27aea3656b87ffcd1b52
Mathieu Gilbert <matnarak@gmail.com>
2012-02-25 09:42:22 -0700

    pages controller only includes items from approved members

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
 class PagesController < ApplicationController
   def index
     # Get the most recent 25 tweets
-    @tweets = Tweet.order("date desc").limit(25)
-    @git_events = GitEvent.order("date desc").limit(25)
-    @blog_posts = BlogPost.order("date desc").limit(25)
+    @tweets = filter(Tweet)
+    @git_events = filter(GitEvent)
+    @blog_posts = filter(BlogPost)
   end
+
+  private
+    def approved_member_ids
+      Member.where(:status => 'approved').map(&:id).join(", ")
+    end
+
+    def filter(model)
+      model.where("member_id IN (#{approved_member_ids})").order("date desc").limit(25)
+    end
 end
