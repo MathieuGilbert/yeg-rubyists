4c02ae8e8ed27ad24c5ca6dfd07e295e3b242a12
Ryan Jones <ryan.michael.jones@gmail.com>
2012-02-26 09:06:00 -0700

    added avatar type to member table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120225191553) do
+ActiveRecord::Schema.define(:version => 20120226155652) do
 
   create_table "avatars", :force => true do |t|
     t.string   "description"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(:version => 20120225191553) do
     t.datetime "updated_at",                                    :null => false
     t.boolean  "admin",                  :default => false
     t.string   "status",                 :default => "pending"
+    t.string   "avatar_type"
   end
 
   add_index "members", ["email"], :name => "index_members_on_email", :unique => true
