3c731ea1e78c0ba03d03c757adbf3685b88ff86f
Ryan Jones <ryan.michael.jones@gmail.com>
2012-04-12 16:27:52 -0600

    asset update

---
+++ public/assets/page_driver.js
@@ -0,0 +1,1 @@
+((function(){jQuery(function(){return setInterval(function(){return router.tweets.fetch({add:!0}),router.git_events.fetch({add:!0}),router.blog_posts.fetch({add:!0})},1e4)})})).call(this);
