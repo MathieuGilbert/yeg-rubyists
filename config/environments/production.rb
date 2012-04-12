6cd364c23912851a03be255a77e8d5747694584f
Ryan Jones <ryan.michael.jones@gmail.com>
2012-04-12 13:59:38 -0600

    asset update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ YegRubyists::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-config.assets.precompile << Rails.root.join("app", "assets")
+  config.assets.precompile << ['*.coffee']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
