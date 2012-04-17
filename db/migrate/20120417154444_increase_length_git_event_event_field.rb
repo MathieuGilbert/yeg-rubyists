32886ad33d9fda456d479b71dd74602a6e257059
Mathieu Gilbert <matnarak@gmail.com>
2012-04-17 10:29:31 -0600

    removed bad migrations

--- db/migrate/20120417154444_increase_length_git_event_event_field.rb
+++
@@ -1,9 +0,0 @@
-class IncreaseLengthGitEventEventField < ActiveRecord::Migration
-  def up
-    change_column :git_events, :event, :text, :limit => 1000
-  end
-
-  def down
-    change_column :git_events, :event, :text, :limit => 255
-  end
-end
--- db/migrate/20120417161318_change_git_events_remove_length_from_events.rb
+++
@@ -1,9 +0,0 @@
-class ChangeGitEventsRemoveLengthFromEvents < ActiveRecord::Migration
-  def up
-    change_column :git_events, :event, :text
-  end
-
-  def down
-    change_column :git_events, :event, :text
-  end
-end
